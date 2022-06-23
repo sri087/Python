d5af6e68feaeedccfa5f405be785ced6b8462354
sri087 <srikanth.susarapu@outlook.com>
2022-06-23 06:41:37 -0400

    insertion sort tests

--- algo/tests/test_sorts.py
+++ algo/tests/test_sorts.py
@@ -3,17 +3,19 @@ import sys
 from numpy import true_divide
 sys.path.append('..')
 
-from src.sorts import bubble_sort
+from src.sorts import bubble_sort, insertion_sort
 from src.searches import binary_search
 
 class TestSorts():
 
-    small_list = [2.5,1,4,6]
-    big_list = [2.5,5.5,3,6,5,1,-10,56,34,26]
+    small_list = [2.5,1,4,6,2.5]
+    big_list = [2.5,5.5,3,6,5,1,-10,56,34,26,26]
 
     odd_big_list = big_list + [72]
 
     def _compare(self, a_list, b_list):
+
+        
 
         if len(a_list) != len(b_list):
             return False
@@ -24,21 +26,19 @@ class TestSorts():
         return True
 
 
-    def test_bubble_sort_small_list(self):
+    def test_bubble_sort(self):
 
         assert self._compare(bubble_sort(self.small_list),sorted(self.small_list)) == True
         assert self._compare(bubble_sort(self.small_list,ascending=False),sorted(self.small_list,reverse=True)) == True
-        assert binary_search(self.small_list, 4) == True
-        assert binary_search(self.small_list, 7) == False
-        assert binary_search(self.small_list, 2.5) == True
-        assert binary_search(self.small_list, 6) == True
-        assert binary_search(self.small_list, 1) == True
+        assert self._compare(bubble_sort(self.big_list),sorted(self.big_list)) == True
+        assert self._compare(bubble_sort(self.big_list,ascending=False),sorted(self.big_list,reverse=True)) == True
 
-    def test_search_big_list(self):
+    def test_insertion_sort(self):
 
-        assert binary_search(self.big_list, -10) == True
-        assert binary_search(self.big_list, 56) == True
-        assert binary_search(self.big_list, 7) == False
+        assert self._compare(insertion_sort(self.small_list),sorted(self.small_list)) == True
+        assert self._compare(insertion_sort(self.small_list,ascending=False),sorted(self.small_list,reverse=True)) == True
+        assert self._compare(insertion_sort(self.big_list),sorted(self.big_list)) == True
+        assert self._compare(insertion_sort(self.big_list,ascending=False),sorted(self.big_list,reverse=True)) == True
 
     def test_search_odd_big_list(self):
 
