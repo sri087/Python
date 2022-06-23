74afc1cd3cacc47af2926e6d94f9f726a28eac4c
sri087 <srikanth.susarapu@outlook.com>
2022-06-23 06:40:53 -0400

    draft insertion sort

--- algo/src/sorts.py
+++ algo/src/sorts.py
@@ -1,3 +1,4 @@
+
 def bubble_sort(to_sort:list, ascending:bool = True) -> list :
 
     list_length = len(to_sort)
@@ -11,3 +12,30 @@ def bubble_sort(to_sort:list, ascending:bool = True) -> list :
                 if to_sort[j] < to_sort[j+1]:
                     to_sort[j],to_sort[j+1] = to_sort[j+1],to_sort[j]
     return to_sort
+
+def insertion_sort(to_sort:list, ascending:bool = True) -> list :
+
+    list_length = len(to_sort)
+
+    for i in range(1, list_length):
+        current_item = to_sort[i]
+        j = i - 1 
+
+        while j >= 0:
+            if ascending:
+                if to_sort[j] > current_item:
+                    to_swap = to_sort[j]
+                    to_sort[j] = current_item
+                    to_sort[j+1] = to_swap
+            else:
+                if to_sort[j] < current_item:
+                    to_swap = to_sort[j]
+                    to_sort[j] = current_item
+                    to_sort[j+1] = to_swap
+            j = j - 1
+
+    return to_sort
+
+
+def merge_sort(to_sort:list, ascending:bool = True) -> list :
+    pass
